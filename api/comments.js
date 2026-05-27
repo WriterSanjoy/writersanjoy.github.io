f9132ae2dcb2d28206a95d6b04ef3e59b2686eba
Sanjoy Mondal <147906763+WriterSanjoy@users.noreply.github.com>
2026-05-28 01:46:36 +0530

    Update comments.js

--- api/comments.js
+++ api/comments.js
@@ -13,7 +13,7 @@ if (
 const owner = process.env.GITHUB_OWNER;
 const repo = process.env.GITHUB_REPO;
 const token = process.env.GITHUB_TOKEN;
-
+console.log(fileData);
 const type =
     req.method === "GET"
         ? req.query.type
