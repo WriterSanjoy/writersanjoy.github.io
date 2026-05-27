1286951867aaf69f878b6448c020f0b737bbfcba
Sanjoy Mondal <147906763+WriterSanjoy@users.noreply.github.com>
2026-05-28 02:10:40 +0530

    Update comments.js

--- api/comments.js
+++ api/comments.js
@@ -2,10 +2,30 @@ export default async function handler(req, res) {
 
   try {
 
+    const owner = process.env.GITHUB_OWNER;
+    const repo = process.env.GITHUB_REPO;
+    const token = process.env.GITHUB_TOKEN;
+
+    const type = req.query.type;
+    const contentId = req.query.id;
+
+    const filePath =
+      `comments/${type}/${contentId}.json`;
+
+    const response = await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/vnd.github+json"
+        }
+      }
+    );
+
+    const data = await response.json();
+
     return res.status(200).json({
-      success:true,
-      method:req.method,
-      query:req.query
+      githubResponse:data
     });
 
   } catch(error){
